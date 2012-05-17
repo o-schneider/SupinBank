b37bd72255328e35d53a5aea4df8cd28f9f1fb7f
Olivier SCHNEIDER <oliwan3dfx@gmail.com>
2012-05-17 12:24:02 +0200

    Reorganize filters

--- src/main/java/com/supinbank/controllers/ListCustomerController.java
+++ src/main/java/com/supinbank/controllers/ListCustomerController.java
@@ -13,7 +13,7 @@ import java.io.Serializable;
  */
 @Named
 @SessionScoped
-public class CustomerController implements Serializable
+public class ListCustomerController implements Serializable
 {
-
+    private
 }
